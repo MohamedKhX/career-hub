90f468642e3e5476988bf70cfa468a039c4342e2
MohamedKhX <muhamedkhx2@gmail.com>
2025-02-20 16:23:50 +0200

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,7 +114,9 @@
                     <svg class="w-10 h-10 text-brand-red" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
                     <span class="text-2xl font-bold">CareerHub</span>
                 </div>
+{{--
                 <p class="text-gray-400">نربط المواهب بالفرص في العالم العربي</p>
+--}}
                 <div class="flex flex-col space-y-2">
                     <a href="mailto:info@careerhub.com" class="text-gray-400 hover:text-brand-red transition-colors duration-300 flex items-center space-x-2 space-x-reverse">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
